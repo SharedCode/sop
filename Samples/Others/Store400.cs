bd4e1d520f931b7e8e01d52f56f533b2fbdeced9
grecinto <gerardorecinto@yahoo.com>
2015-10-11 11:13:12 -0700

    Store400 persistence is switched to use SOP Persistent class.

--- Samples/Others/Store400.cs
+++ Samples/Others/Store400.cs
@@ -1,85 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Sop.Samples
 {
     public class Store400 : Sample
     {
-        #region Record definitions & key comparers
-        public class PersonKey
+        public class Person : Sop.Persistence.Persistent
         {
+            public int PersonId;
             public string FirstName;
             public string LastName;
-        }
-        public class Person
-        {
-            public PersonKey Key;
-            public int PersonID;
             public string PhoneNumber;
-        }
-        public class PersonComparer : IComparer<PersonKey>
-        {
-            public int Compare(PersonKey x, PersonKey y)
-            {
-                int i = string.Compare(x.FirstName, y.FirstName);
-                if (i == 0)
-                    i = string.Compare(x.LastName, y.LastName);
-                return i;
-            }
-        }
-        #endregion
 
-		/// <summary>
-		/// Sample code that tests creation and population of 400 tables with 5000 records each.
-		/// </summary>
+            #region Pack/Unpack
+            public override void Pack(BinaryWriter writer)
+            {
+                writer.Write(PersonId);
+                writer.Write(FirstName);
+                writer.Write(LastName);
+                writer.Write(PhoneNumber);
+            }
+
+            public override void Unpack(BinaryReader reader)
+            {
+                PersonId = reader.ReadInt32();
+                FirstName = reader.ReadString();
+                LastName = reader.ReadString();
+                PhoneNumber = reader.ReadString();
+            }
+            #endregion
+        }
+        /// <summary>
+        /// Sample code that tests creation and population of 400 tables with 5000 records each.
+        /// </summary>
         public void Run()
         {
             Console.WriteLine("{0}: Store400 demo started...", DateTime.Now);
             const int CollCount = 50;
+            const string NameTemplate = "SystemFile/People{0}";
 
             using (var Server = new ObjectServer(ServerFilename))
             {
-                // Create empty stores to pre-allocate data segments.
-                // Doing segments' pre-allocation proved to cause faster Transaction commits.
-                bool newlyCreatedStores = false;
                 for (int i = 0; i < CollCount; i++)
                 {
-                    string CollectionName = string.Format("SystemFile/People{0}", i);
-                    var store = Server.StoreNavigator.GetStore<long, Person>(CollectionName);
-                    if (store.Count == 0)
-                    {
-                        newlyCreatedStores = true;
-                    }
+                    string CollectionName = string.Format(NameTemplate, i);
+                    var store = Server.StoreNavigator.GetStorePersistentValue<long, Person>(CollectionName);
                 }
-                if (newlyCreatedStores)
-                {
-                    if (Server.Transaction != null)
-                        Server.Commit();
-                    Server.BeginTransaction();
-                }
-                // end of Store create.
-
+                Server.CycleTransaction();
                 for (int i = 0; i < CollCount; i++)
                 {
-                    string CollectionName = string.Format("SystemFile/People{0}", i);
-                    var store = Server.StoreNavigator.GetStore<long, Person>(CollectionName);
+                    string CollectionName = string.Format(NameTemplate, i);
+                    var store = Server.StoreNavigator.GetStorePersistentValue<long, Person>(CollectionName);
                     if (store.Count == 0)
                     {
                         Populate(store);
                         if (i > 0 && i % 10 == 0)
                         {
-                            if (Server.Transaction != null)
-                                Server.Commit();
-                            Server.BeginTransaction();
+                            // no need to cycle(commit/begin) trans, but just to 
+                            // simulate highly transactional app...
+                            Server.CycleTransaction();
                         }
                     }
                 }
                 for (int i = 0; i < CollCount; i++)
                 {
-                    string CollectionName = string.Format("SystemFile/People{0}", i);
+                    string CollectionName = string.Format(NameTemplate, i);
                     if (Server.StoreNavigator.Contains(CollectionName))
                     {
-                        var store = Server.StoreNavigator.GetStore<long, Person>(CollectionName);
+                        var store = Server.StoreNavigator.GetStorePersistentValue<long, Person>(CollectionName);
                         ReadAll(store);
                     }
                 }
@@ -87,26 +76,22 @@ namespace Sop.Samples
 
             Console.WriteLine("{0}: Store400 demo ended...", DateTime.Now);
         }
-        void Populate(ISortedDictionary<long, Person> PeopleStore)
+        void Populate(ISortedDictionary<long, Person> PeopleStore, int count = MaxCount, int seed = 0)
         {
-            for (int i = 0; i < MaxCount; i++)
+            for (int i = seed; i < count; i++)
             {
                 int pid = (int)PeopleStore.GetNextSequence();
                 Person p = new Person()
                 {
-                    PersonID = pid,
-                    Key = new PersonKey()
-                    {
-                        FirstName = string.Format("Joe{0}", pid),
-                        LastName = string.Format("Peter{0}", pid)
-                    },
+                    PersonId = pid,
+                    FirstName = string.Format("Joe{0}", pid),
+                    LastName = string.Format("Peter{0}", pid),
                     PhoneNumber = "510-555-9999"
                 };
-                PeopleStore.Add(p.PersonID, p);
+                PeopleStore.Add(p.PersonId, p);
                 if (i > 0 && i % 10000 == 0)
                 {
-                    PeopleStore.File.Server.Commit();
-                    PeopleStore.File.Server.BeginTransaction();
+                    PeopleStore.File.Server.CycleTransaction();
                 }
             }
         }
