8c6c5d5a912c4766a227dc0b0039e52e89a0b196
Gerardo Recinto <gerardorecinto@yahoo.com>
2023-10-31 22:45:33 -0700

    Fleshed out the add item method including "vacant" slot detection & item distribution part of load balancing.

--- sop_test.go
+++ sop_test.go
@@ -1,16 +1,36 @@
 package sop
 
 import (
-	"testing"
 	"encoding/json"
+	"fmt"
+	"testing"
+
 	"github.com/SharedCode/sop/store/in_memory"
 )
 
 func TestBtreeBasic(t *testing.T) {
 	btree, _ := in_memory.NewStore[string, string]()
 	btree.Add("foo", "bar")
-	if ok, _ := btree.FindOne("foo", true); !ok || btree.GetCurrentValue() != "bar" {
-		ba,_ := json.Marshal(btree)
+	ba, _ := json.Marshal(btree)
+	if ok, _ := btree.FindOne("foo", true); ok && btree.GetCurrentValue() == "bar" {
+		fmt.Printf("btree: %s", string(ba))
+	} else {
+		t.Errorf("btree: %s", string(ba))
+		t.Errorf("Did not find foo's bar.")
+	}
+}
+
+// TODO: support node breakup! this test fails until such feature is implemented.
+func TestBtreeAddLoop(t *testing.T) {
+	btree, _ := in_memory.NewStore[string, string]()
+	const n = 5
+	for i := 0; i < n; i++ {
+		btree.Add(fmt.Sprintf("foo%d", i), fmt.Sprintf("bar%d", i))
+	}
+	ba, _ := json.Marshal(btree)
+	if ok, _ := btree.FindOne("foo1", true); ok && btree.GetCurrentValue() == "bar1" {
+		fmt.Printf("btree: %s", string(ba))
+	} else {
 		t.Errorf("btree: %s", string(ba))
 		t.Errorf("Did not find foo's bar.")
 	}
