b3fadafe4e2a0adb2c8db978a621aa304e0cfb95
Gerardo Recinto <gerardorecinto@yahoo.com>
2023-11-01 19:28:15 -0700

    Added verbiage on Transaction Manager about surfacing repository interfaces.

--- store/in_memory/transaction_manager.go
+++ store/in_memory/transaction_manager.go
@@ -2,11 +2,18 @@ package in_memory
 
 import "github.com/SharedCode/sop/btree"
 
-// in-memory transaction manager just relays CRUD actions to the actual NodeRepository.
+// in-memory transaction manager just relays CRUD actions to the actual in-memory NodeRepository.
 type transaction_manager[TK btree.Comparable, TV any] struct {
 	storeInterface   *btree.StoreInterface[TK, TV] `json:"-"`
 }
 
+// Transaction Manager surfaces a StoreInterface that which, knows how to reconcile/merge
+// changes in a transaction session with the actual destination storage.
+// Idea here is, to let the algorithms be abstracted with the backend store. To the b-tree
+// it feels like it only uses normal repositories but in actuality, these repositories
+// are facade for the transaction manager, so it can reconcile/merge with the backend
+// during transaction commit. A very simple model, but powerful/complete control on
+// the data changes & necessary merging with backend storage.
 func newTransactionManager[TK btree.Comparable, TV any]() *transaction_manager[TK,TV] {
 	si := btree.StoreInterface[TK, TV]{
 		NodeRepository:      newNodeRepository[TK, TV](),
