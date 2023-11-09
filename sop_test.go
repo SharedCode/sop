91a525f3a49e28e9fcd82c73a94929a9a9bd9f31
Gerardo Recinto <gerardorecinto@yahoo.com>
2023-11-09 00:01:34 -0800

    Added an exhaustive B-Tree management tests for stability checks. Test Passed. :)

--- sop_test.go
+++ sop_test.go
@@ -13,39 +13,90 @@ func TestBtreeBasic(t *testing.T) {
 }
 
 func TestBtreeNilChild(t *testing.T) {
-	n := 100000
-	fmt.Printf("Nil child %d loop test\n\n", n)
+	max := 100000
+	fmt.Printf("Nil child %d loop test\n\n", max)
 	b3, _ := in_memory.NewBtree[string, string](false)
-	for i := 0; i < n; i++ {
-		k := fmt.Sprintf("foo%d", i)
-		v := fmt.Sprintf("bar%d", i)
-		b3.Add(k, v)
+
+	tests := []struct {
+		name string
+		startRange int
+		endRange int
+		action int
+	}{
+		{
+			name: "populate",
+			startRange: 0,
+			endRange: max,
+			action: 1,	// add
+		},
+		{
+			name: "find1",
+			startRange: 0,
+			endRange: max,
+			action: 2,	// find
+		},
+		{
+			name: "remove1",
+			startRange: 450,
+			endRange: 800,
+			action: 3, // remove
+		},
+		{
+			name: "readd deleted items",
+			startRange: 450,
+			endRange: 800,
+			action: 1,
+		},
+		{
+			name: "findAll1",
+			startRange: 0,
+			endRange: max,
+			action: 2,
+		},
+		{
+			name: "remove2",
+			startRange: 5000,
+			endRange: 10000,
+			action: 3,
+		},
+		{
+			name: "readd deleted items2",
+			startRange: 5000,
+			endRange: 10000,
+			action: 1,
+		},
+		{
+			name: "findAll2",
+			startRange: 0,
+			endRange: max,
+			action: 2,	// find
+		},
 	}
 
-	removeStart := 450
-	removeEnd := 500
+	for _,test := range tests {
+		t.Logf("Test %s started.", test.name)
+		for i := test.startRange; i < test.endRange; i++ {
+			k := fmt.Sprintf("foo%d", i)
+			v := fmt.Sprintf("bar%d", i)
 
-	for i := removeStart; i < removeEnd; i++ {
-		k := fmt.Sprintf("foo%d", i)
-		if ok,_ := b3.Remove(k); !ok {
-			t.Errorf("Failed deleting item with key %s.\n", k)
+			switch test.action {
+			case 1:
+				if ok,_ := b3.Add(k,v); !ok {
+					t.Errorf("Failed Add item with key %s.\n", k)
+				}
+			case 2:
+				if ok,_ := b3.FindOne(k, true); !ok {
+					t.Errorf("Failed FindOne item with key %s.\n", k)
+				}
+			case 3:
+				if ok,err := b3.Remove(k); !ok {
+					t.Errorf("Failed Delete item with key %s, error: %v\n", k, err)
+				}
+			}
 		}
+		t.Logf("Test %s ended.", test.name)
 	}
-
-	for i := removeStart; i < removeEnd; i++ {
-		k := fmt.Sprintf("foo%d", i)
-		v := fmt.Sprintf("bar%d", i)
-		b3.Add(k,v)
-	}
-
-	for i := 0; i < n; i++ {
-		k := fmt.Sprintf("foo%d", i)
-		v := fmt.Sprintf("bar%d", i)
-		if ok, _ := b3.FindOne(k, true); !ok || b3.GetCurrentValue() != v {
-			fmt.Printf("Not found key:%s, value: %s\n", k, v)
-		}
-	}
-	fmt.Printf("Nil child %d loop test end.\n\n", n)
+	fmt.Printf("Nil child %d loop test ended.\n\n", max)
 }
 
 func TestBtreePromoteAndDistributeStability(t *testing.T) {
