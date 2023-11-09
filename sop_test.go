f71656013c74120410e2b9988ce1cc3895356c83
Gerardo Recinto <gerardorecinto@yahoo.com>
2023-11-09 00:07:15 -0800

    Removed unnecessary tests.

--- sop_test.go
+++ sop_test.go
@@ -1,18 +1,13 @@
 package sop
 
 import (
-	"encoding/json"
 	"fmt"
 	"testing"
 
 	"github.com/SharedCode/sop/store/in_memory"
 )
 
-func TestBtreeBasic(t *testing.T) {
-	testBtreeAddLoop(t, 200)
-}
-
-func TestBtreeNilChild(t *testing.T) {
+func TestBtree(t *testing.T) {
 	max := 100000
 	fmt.Printf("Nil child %d loop test\n\n", max)
 	b3, _ := in_memory.NewBtree[string, string](false)
@@ -97,44 +92,4 @@ func TestBtreeNilChild(t *testing.T) {
 		t.Logf("Test %s ended.", test.name)
 	}
 	fmt.Printf("Nil child %d loop test ended.\n\n", max)
-}
-
-func TestBtreePromoteAndDistributeStability(t *testing.T) {
-	n := 100000
-	fmt.Printf("Promote & Distribute(P&D) %d loop test\n\n", n)
-	b3, _ := in_memory.NewBtree[string, string](false)
-	for i := 0; i < n; i++ {
-		k := fmt.Sprintf("foo%d", i)
-		v := fmt.Sprintf("bar%d", i)
-		b3.Add(k, v)
-		if ok, _ := b3.FindOne(k, true); !ok || b3.GetCurrentValue() != v {
-			fmt.Printf("Not found key:%s, value: %s\n", k, v)
-		}
-	}
-	for i := 0; i < n; i++ {
-		k := fmt.Sprintf("foo%d", i)
-		v := fmt.Sprintf("bar%d", i)
-		if ok, _ := b3.FindOne(k, true); !ok || b3.GetCurrentValue() != v {
-			fmt.Printf("Not found key:%s, value: %s\n", k, v)
-		}
-	}
-	fmt.Printf("P&D %d loop test end.\n\n", n)
-}
-
-func testBtreeAddLoop(t *testing.T, n int) {
-	fmt.Printf("btree %d loop test\n\n", n)
-	b3, _ := in_memory.NewBtree[string, string](false)
-	for i := 0; i < n; i++ {
-		k := fmt.Sprintf("foo%d", i)
-		v := fmt.Sprintf("bar%d", i)
-		b3.Add(k, v)
-		ba, _ := json.Marshal(b3)
-		if ok, _ := b3.FindOne(k, true); ok && b3.GetCurrentValue() == v {
-			//fmt.Printf("btree: %s\n", string(ba))
-		} else {
-			t.Errorf("btree: %s\n", string(ba))
-			t.Errorf("Did not find %s's %s.\n", k, v)
-		}
-	}
-	fmt.Printf("btree %d loop test end.\n\n", n)
 }
