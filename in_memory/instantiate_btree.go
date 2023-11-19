a503bc9832c2eb7fb09775a607b42d8bdec64787
Gerardo Recinto <gerardorecinto@yahoo.com>
2023-11-19 09:17:46 -0800

    Added NewBtreeWithNoWrapper to allow in-memory btree reuse in unit tests to mock the V2 version. In preparation for writing the V2 SOP.

--- in_memory/instantiate_btree.go
+++ in_memory/instantiate_btree.go
@@ -67,6 +67,17 @@ func NewBtree[TK btree.Comparable, TV any](isUnique bool) (BtreeInterface[TK, TV
 	}
 }
 
+// NewBtree will create an in-memory B-Tree & its required data stores. You can use it to store
+// and access key/value pairs similar to a map but which, sorts items & allows "range queries".
+// This will return btree instance that has no wrapper, thus, methods have error in return where appropriate.
+// Handy for using in-memory b-tree for writing unit tests to mock the "Enterprise" V2 version.
+func NewBtreeWithNoWrapper[TK btree.Comparable, TV any](isUnique bool) (btree.BtreeInterface[TK, TV]) {
+	transactionManager := newTransactionManager[TK,TV]()
+	s := btree.NewStore("", itemsPerNode, isUnique, true)
+	transactionManager.storeInterface.StoreRepository.Add(s)
+	return btree.NewBtree[TK, TV](s, transactionManager.storeInterface)
+}
+
 // Add adds an item to the b-tree and does not check for duplicates.
 func (b3 inmemoryBtree[TK, TV]) Add(key TK, value TV) bool {
 	ok,_ := b3.btree.Add(key, value)
