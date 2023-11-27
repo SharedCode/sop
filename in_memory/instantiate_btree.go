a5824a3aa4a2f39ca972dff7b4b85519fcdec5d0
Gerardo Recinto <gerardorecinto@yahoo.com>
2023-11-26 20:51:58 -0800

    Added comments & restored the BTreeInterface for inmemory that got refactored in previous checkin.

--- in_memory/instantiate_btree.go
+++ in_memory/instantiate_btree.go
@@ -2,6 +2,49 @@ package in_memory
 
 import "github.com/SharedCode/sop/btree"
 
+// BtreeInterface defines publicly callable methods of Btree in-memory.
+// NOTE: this is synonymous to the btree.BtreeInterface but with methods removed of error in return.
+// Because in-memory will not produce any error during access, thus, it can be simplified so code will not need
+// to bother with the 2nd (error) return.
+type BtreeInterface[TK btree.Comparable, TV any] interface {
+	// Add adds an item to the b-tree and does not check for duplicates.
+	Add(key TK, value TV) bool
+	// AddIfNotExist adds an item if there is no item matching the key yet.
+	// Otherwise, it will do nothing and return false, for not adding the item.
+	// This is useful for cases one wants to add an item without creating a duplicate entry.
+	AddIfNotExist(key TK, value TV) bool
+	// FindOne will search Btree for an item with a given key. Return true if found,
+	// otherwise false. firstItemWithKey is useful when there are items with same key.
+	// true will position pointer to the first item with the given key,
+	// according to key ordering sequence.
+	FindOne(key TK, firstItemWithKey bool) bool
+	// GetCurrentKey returns the current item's key.
+	GetCurrentKey() TK
+	// GetCurrentValue returns the current item's value.
+	GetCurrentValue() TV
+	// Update finds the item with key and update its value to the value argument.
+	Update(key TK, value TV) bool
+	// UpdateCurrentItem will update the Value of the current item.
+	// Key is read-only, thus, no argument for the key.
+	UpdateCurrentItem(newValue TV) bool
+	// Remove will find the item with a given key then remove that item.
+	Remove(key TK) bool
+	// RemoveCurrentItem will remove the current key/value pair from the store.
+	RemoveCurrentItem() bool
+
+	// Cursor like "move" functions. Use the CurrentKey/CurrentValue to retrieve the
+	// "current item" details(key &/or value).
+	MoveToFirst() bool
+	MoveToLast() bool
+	MoveToNext() bool
+	MoveToPrevious() bool
+
+	// IsUnique returns true if B-Tree is specified to store items with Unique keys, otherwise false.
+	// Specifying uniqueness base on key makes the B-Tree permanently set. If you want just a temporary
+	// unique check during Add of an item, then you can use AddIfNotExist method for that.
+	IsUnique() bool
+}
+
 type inmemoryBtree[TK btree.Comparable, TV any] struct {
 	btree btree.BtreeInterface[TK, TV]
 }
@@ -11,7 +54,7 @@ const itemsPerNode = 8
 
 // NewBtree will create an in-memory B-Tree & its required data stores. You can use it to store
 // and access key/value pairs similar to a map but which, sorts items & allows "range queries".
-func NewBtree[TK btree.Comparable, TV any](isUnique bool) inmemoryBtree[TK, TV] {
+func NewBtree[TK btree.Comparable, TV any](isUnique bool) BtreeInterface[TK, TV] {
 	transactionManager := newTransactionManager[TK, TV]()
 	s := btree.NewStore("", itemsPerNode, isUnique, true)
 	transactionManager.storeInterface.StoreRepository.Add(s)
