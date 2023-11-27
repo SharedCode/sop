698e13276ff177df4bc22ce6f1e0041eea8e1b4b
Gerardo Recinto <gerardorecinto@yahoo.com>
2023-11-26 21:01:18 -0800

    Added unit test to demonstrate passing (generic) in_memory btree interface to a sub-function.

--- sop_comparer_test.go
+++ sop_comparer_test.go
@@ -9,27 +9,28 @@ import (
 
 type personKey struct {
 	firstname string
-	lastname string
+	lastname  string
 }
 
 type person struct {
 	personKey
 	gender string
-	email string
-	phone string
+	email  string
+	phone  string
 }
+
 func newPerson(fname string, lname string, gender string, email string, phone string) person {
-	return person {
+	return person{
 		personKey: personKey{
 			firstname: fname,
-			lastname: lname,
+			lastname:  lname,
 		},
 		gender: gender,
-		email: email,
-		phone: phone,
+		email:  email,
+		phone:  phone,
 	}
 }
-func (x personKey)Compare(other interface{}) int {
+func (x personKey) Compare(other interface{}) int {
 	y := other.(personKey)
 	i := cmp.Compare[string](x.lastname, y.lastname)
 	if i != 0 {
@@ -49,5 +50,14 @@ func Test_PersonLookup(t *testing.T) {
 		t.Logf("Person w/ key %v found.", b3.GetCurrentKey())
 	}
 
+	if !passBtreeAround(b3) {
+		t.Logf("passBtreeAround(b3) failed, got false, want true.")
+	}
+
 	t.Log("Btree demo, used as a person struct lookup end.\n")
+}
+
+func passBtreeAround(b3 sop.BtreeInterface[personKey, person]) bool {
+	key := personKey{firstname: "joe", lastname: "krueger"}
+	return b3.FindOne(key, false)
 }
