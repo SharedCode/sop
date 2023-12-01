08b6aea217911ed6255e504f3de692bc319275d0
Gerardo Recinto <gerardorecinto@yahoo.com>
2023-12-01 11:29:06 -0800

    Renamed "in_aws" to "in_cas_s3".

--- in_cas_s3/instantiate_btree.go
+++ in_cas_s3/instantiate_btree.go
@@ -3,7 +3,8 @@ package in_aws
 import "github.com/SharedCode/sop/btree"
 
 // NewBtree will create B-Tree with data persisted in backend store, e.g. - AWS storage services.
-func NewBtree[TK btree.Comparable, TV any](name string, slotLength int, isUnique bool, isValueDataInNodeSegment bool, t Transaction) btree.BtreeInterface[TK, TV] {
+func NewBtree[TK btree.Comparable, TV any](name string, slotLength int, isUnique bool,
+	  isValueDataInNodeSegment bool, t Transaction) btree.BtreeInterface[TK, TV] {
 	transactionManager := newTransactionManager[TK, TV]()
 	s := btree.NewStore(name, slotLength, isUnique, true)
 	transactionManager.storeInterface.StoreRepository.Add(s)
