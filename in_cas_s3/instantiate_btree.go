e5082c57e21eb8cef090ab35e68f2946510bd529
Gerardo Recinto <gerardorecinto@yahoo.com>
2023-12-07 15:05:54 -0800

    (Refactor)Removed transaction_manager and merged its code to instantiate_btree.

--- in_cas_s3/instantiate_btree.go
+++ in_cas_s3/instantiate_btree.go
@@ -5,8 +5,13 @@ import "github.com/SharedCode/sop/btree"
 // NewBtree will create B-Tree with data persisted in backend store, e.g. - AWS storage services.
 func NewBtree[TK btree.Comparable, TV any](name string, slotLength int, isUnique bool,
 	isValueDataInNodeSegment bool, t Transaction) btree.BtreeInterface[TK, TV] {
-	transactionManager := newTransactionManager[TK, TV]()
+	si := StoreInterface[TK, TV]{
+		RecyclerRepository:  newRecycler(), // shared globally.
+		VirtualIdRepository: newVirtualIdRepository(),
+		StoreRepository:     newStoreRepository(), // shared globally.
+	}
+	si.NodeRepository = newNodeRepository[TK, TV]()
 	s := btree.NewStore(name, slotLength, isUnique, true)
-	transactionManager.storeInterface.StoreRepository.Add(s)
-	return btree.NewBtree[TK, TV](s, &transactionManager.storeInterface.StoreInterface)
+	si.StoreRepository.Add(s)
+	return btree.NewBtree[TK, TV](s, &si.StoreInterface)
 }
