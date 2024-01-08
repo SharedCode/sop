abee97304a824cb17a57cf6fb54d0bbd8afc10ab
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-01-08 12:41:31 -0800

    Safekeep before we refactor the transaction & Btree for multi entity type support.

---
+++ in_red_ck/comparer_test.go
@@ -0,0 +1,75 @@
+package in_red_ck
+
+import (
+	"cmp"
+	"testing"
+)
+
+type PersonKey struct {
+	Firstname string
+	Lastname  string
+}
+
+type Person struct {
+	Gender string
+	Email  string
+	Phone  string
+}
+
+func newPerson(fname string, lname string, gender string, email string, phone string) (PersonKey, Person) {
+	return PersonKey{
+		Firstname: fname,
+		Lastname:  lname,
+	},
+	Person{
+		Gender: gender,
+		Email:  email,
+		Phone:  phone,
+	}
+}
+func (x PersonKey) Compare(other interface{}) int {
+	y := other.(PersonKey)
+	i := cmp.Compare[string](x.Lastname, y.Lastname)
+	if i != 0 {
+		return i
+	}
+	return cmp.Compare[string](x.Firstname, y.Firstname)
+}
+
+func Test_SimpleAddPerson(t *testing.T) {
+	t.Logf("Transaction story, single b-tree, person record test.\n")
+	trans, err := NewTransaction(true, -1)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	trans.Begin()
+
+	pk, p := newPerson("joe", "krueger", "male", "email", "phone")
+
+	b3, _ := NewBtree[PersonKey, Person](ctx, "persondb", 4, false, false, false, "", trans)
+	if ok, err := b3.Add(ctx, pk, p); !ok || err != nil {
+			t.Errorf("Add('joe') failed, got(ok, err) = %v, %v, want = true, nil.", ok, err)
+			trans.Rollback(ctx)
+		return
+	}
+
+	if ok, err := b3.FindOne(ctx, pk, false); !ok || err != nil {
+		t.Errorf("FindOne('joe',false) failed, got(ok, err) = %v, %v, want = true, nil.", ok, err)
+		trans.Rollback(ctx)
+		return
+	}
+	if k, err := b3.GetCurrentKey(ctx); k.Firstname != pk.Firstname || err != nil {
+		t.Errorf("GetCurrentKey() failed, got = %v, %v, want = 1, nil.", k, err)
+		trans.Rollback(ctx)
+		return
+	}
+	if v, err := b3.GetCurrentValue(ctx); v.Phone != p.Phone || err != nil {
+		t.Errorf("GetCurrentValue() failed, got = %v, %v, want = 1, nil.", v, err)
+		trans.Rollback(ctx)
+		return
+	}
+	t.Logf("Successfully added & found item with key 'joe'.")
+	if err := trans.Commit(ctx); err != nil {
+		t.Errorf("Commit returned error, details: %v.", err)
+	}
+}
--- in_red_ck/store_interfaces.go
+++ in_red_ck/store_interfaces.go
@@ -7,43 +7,8 @@ import (
 // StoreInterface contains different repositories needed/used by B-Tree to manage/access its data/objects.
 type StoreInterface[TK btree.Comparable, TV any] struct {
 	btree.StoreInterface[TK, TV]
-
 	// Non-generics item action tracker, used in transaction commit to process modified Items.
 	backendItemActionTracker *itemActionTracker
 	// Non-generics node repository, used in transaction commit to process modified Nodes.
 	backendNodeRepository *nodeRepository
 }
-
-// TODO: uncomment and reuse anything below as needed. (initial design artifacts)
-
-// type EntityType uint
-// const (
-// 	// BTreeNode is the entity type of the B-Tree Node.
-// 	BTreeNode = iota
-// 	// ValuePart is the entity type of the value part in the key/value pair
-// 	// that a B-Tree supports persistence & access.
-// 	ValuePart
-// )
-
-// type NodeDataBlocks struct {
-// 	Id               Handle
-// 	SlotDataBlock    []byte
-// 	SlotDataBlockIds []btree.UUID
-// 	Children         []btree.UUID
-// 	count            int
-// 	Version          int
-// 	IsDeleted        bool
-// }
-
-// type SlotValueDataBlocks struct {
-// 	Id                Handle
-// 	Value             []byte
-// 	ValueDataBlockIds []btree.UUID
-// 	IsDeleted         bool
-// }
-
-// type SlotValue struct {
-// 	Id        Handle
-// 	Value     []byte
-// 	IsDeleted bool
-// }
