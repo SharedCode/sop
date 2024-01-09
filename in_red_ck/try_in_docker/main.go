c24d9b47d725edbe9d4bc3588371821329b661b7
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-01-09 00:14:58 -0800

    trydocker sync.

--- in_red_ck/try_in_docker/main.go
+++ in_red_ck/try_in_docker/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/SharedCode/sop/btree"
 	"github.com/SharedCode/sop/in_red_ck"
 	cas "github.com/SharedCode/sop/in_red_ck/cassandra"
+	"github.com/SharedCode/sop/in_red_ck/kafka"
 	"github.com/SharedCode/sop/in_red_ck/redis"
 	"github.com/gocql/gocql"
 )
@@ -25,7 +26,7 @@ var redisConfig = redis.Options{
 }
 
 func main() {
-	if err := in_red_ck.Initialize(cassConfig, redisConfig); err != nil {
+	if err := in_red_ck.Initialize(cassConfig, redisConfig, kafka.DefaultConfig); err != nil {
 		writeAndExit(err.Error())
 	}
 	storeInfo := *btree.NewStoreInfo("foobar", 4, true, true, true, "")
