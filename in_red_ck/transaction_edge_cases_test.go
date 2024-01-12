15c18e644f6507286a3de34aa5e513ca70fcaea4
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-01-11 18:55:20 -0800

    Plan on transaction edge cases testing.

---
+++ in_red_ck/transaction_edge_cases_test.go
@@ -0,0 +1,9 @@
+package in_red_ck
+
+/*
+  - Two transactions updating same item.
+  - Two transactions updating different items with collision on 1 item.
+  - Two transactions updating different items with no collision but items' keys are sequential/contiguous between the two.
+  - One transaction updates a colliding item in 1st and a 2nd trans, updates the colliding item as last.
+  - [add more test cases here...]
+*/
