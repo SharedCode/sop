b8ce4c14652eb9adfdca36ae0f3fdd2e358a2339
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-01-15 19:14:14 -0800

    New UUID function ensures no panic is generated, by retrying & sleeping to allow the generator to recover.

--- btree/uuid.go
+++ btree/uuid.go
@@ -2,15 +2,26 @@ package btree
 
 import (
 	"bytes"
+	"time"
+
 	"github.com/google/uuid"
 )
 
 // UUID type.
 type UUID uuid.UUID
 
-// NewUUID returns a new UUID.
+// NewUUID returns a new UUID. Will retry after sleep of 1 milli if an error occurs.
 func NewUUID() UUID {
-	return UUID(uuid.New())
+	// In the case of generating new UUID errored, we just need to retry because
+	// generating UUID is a must.
+	for {
+		id, err := uuid.NewRandom()
+		if err == nil {
+			return UUID(id)
+		}
+		// Sleep 1 millisecond then retry to generate new UUID.
+		time.Sleep(time.Duration(1*time.Millisecond))
+	}
 }
 
 // NillUUID is an empty UUID.
