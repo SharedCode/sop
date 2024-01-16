6859e4aeed2f275015670ee8d2685ca769f33f1b
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-01-15 18:54:41 -0800

    Refactor to another file Redis & Cassandra "init"/shutdown functions.

---
+++ in_red_ck/init_red_c.go
@@ -0,0 +1,29 @@
+package in_red_ck
+
+import (
+	cas "github.com/SharedCode/sop/in_red_ck/cassandra"
+	"github.com/SharedCode/sop/in_red_ck/redis"
+)
+
+// Assign the configs & open connections to different sub-systems used by this package.
+// Example, connection to Cassandra, Redis.
+func Initialize(cassandraConfig cas.Config, redisConfig redis.Options) error {
+	if _, err := cas.OpenConnection(cassandraConfig); err != nil {
+		return err
+	}
+	if _, err := redis.OpenConnection(redisConfig); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Returns true if components required were initialized, false otherwise.
+func IsInitialized() bool {
+	return cas.IsConnectionInstantiated() && redis.IsConnectionInstantiated()
+}
+
+// Shutdown or closes all connections used in this package.
+func Shutdown() {
+	cas.CloseConnection()
+	redis.CloseConnection()
+}
--- in_red_ck/manage_btree.go
+++ in_red_ck/manage_btree.go
@@ -6,31 +6,8 @@ import (
 
 	"github.com/SharedCode/sop/btree"
 	cas "github.com/SharedCode/sop/in_red_ck/cassandra"
-	"github.com/SharedCode/sop/in_red_ck/redis"
 )
 
-// Assign the configs & open connections to different sub-systems used by this package.
-// Example, connection to Cassandra, Redis.
-func Initialize(cassandraConfig cas.Config, redisConfig redis.Options) error {
-	if _, err := cas.OpenConnection(cassandraConfig); err != nil {
-		return err
-	}
-	if _, err := redis.OpenConnection(redisConfig); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Returns true if components required were initialized, false otherwise.
-func IsInitialized() bool {
-	return cas.IsConnectionInstantiated() && redis.IsConnectionInstantiated()
-}
-
-// Shutdown or closes all connections used in this package.
-func Shutdown() {
-	cas.CloseConnection()
-	redis.CloseConnection()
-}
 
 // Removes B-Tree with a given name from the backend storage. This involves dropping tables
 // (registry & node blob) that are permanent action and thus, 'can't get rolled back.
