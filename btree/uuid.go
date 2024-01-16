65686f4ce3c47da783d7e21997d9093faf0955b7
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-01-15 19:28:54 -0800

    10 retries if UUID can't be generated.

--- btree/uuid.go
+++ btree/uuid.go
@@ -11,18 +11,22 @@ import (
 type UUID uuid.UUID
 
 // NewUUID returns a new UUID. Will retry after sleep of 1 milli if an error occurs.
-// Guaranteed no panic by sleeping/retry.
+// Guaranteed no panic by sleeping/retry, and only does after exhausting 10 attempts.
 func NewUUID() UUID {
 	// In the case of generating new UUID errored, we just need to retry because
 	// generating UUID is a must.
-	for {
-		id, err := uuid.NewRandom()
+	var err error
+	for i :=0; i < 10; i++ {
+		var id uuid.UUID
+		id, err = uuid.NewRandom()
 		if err == nil {
 			return UUID(id)
 		}
 		// Sleep 1 millisecond then retry to generate new UUID.
 		time.Sleep(time.Duration(1 * time.Millisecond))
 	}
+	// Panic if still can't generate UUID after 10 retries. Should never happen but in case.
+	panic(err)
 }
 
 // NillUUID is an empty UUID.
