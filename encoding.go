cf25f5bc83df5db6a641dd84bad6467224bdd1ea
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-01-18 06:58:26 -0800

    Comment change of new Marshaler interface..

--- encoding.go
+++ encoding.go
@@ -6,7 +6,9 @@ import (
 
 // Marshaler interface specifies encoding to byte array and back to the object.
 type Marshaler interface {
+	// Encodes any object to byte array.
 	Marshal(v any) ([]byte, error)
+	// Decodes byte array back to its Object type.
 	Unmarshal(data []byte, v any) error
 }
 
@@ -17,12 +19,12 @@ func NewMarshaler() Marshaler {
 	return &defaultMardhaller{}
 }
 
-// Marshal encodes any object to byte array.
+// Encodes any object to a byte array.
 func (m defaultMardhaller)Marshal(v any) ([]byte, error) {
 	return json.Marshal(v)
 }
 
-// Unmarshal encodes a byte array back to any object.
+// Decodes a byte array back to its Object type.
 func (m defaultMardhaller)Unmarshal(data []byte, v any) error {
 	return json.Unmarshal(data, v)
 }
