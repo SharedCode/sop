86e8d0b3ce30aed3a96b6836b79ee0cb26da6332
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-01-18 14:13:36 -0800

    More cleanup.

--- in_red_ck/integration_tests/delete_btree_test.go
+++ in_red_ck/integration_tests/delete_btree_test.go
@@ -2,15 +2,17 @@ package in_red_ck
 
 import (
 	"testing"
+
+	"github.com/SharedCode/sop/in_red_ck"
 )
 
 // Add Test_ prefix if you want to run this test.
 // It drops the blob & registry tables of the B-Tree, thus, the test was removed from the set.
 func DeleteBTree(t *testing.T) {
-	if err := RemoveBtree(ctx, "fooStore"); err != nil {
+	if err := in_red_ck.RemoveBtree(ctx, "fooStore"); err != nil {
 		t.Error(err)
 	}
-	if err := RemoveBtree(ctx, "persondb"); err != nil {
+	if err := in_red_ck.RemoveBtree(ctx, "persondb"); err != nil {
 		t.Error(err)
 	}
 }
