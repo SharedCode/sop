f20b7f6bde406a3f0c643ae519b762fed37d0392
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-02-09 15:19:33 -0800

    Refinements on Streaming Data Store. Kept ability to search and stream down any entries' data chunk. I thought it is a useful feature for streaming domain, ability to resume and continue streaming from a fragment or chunk position. :)

--- streaming_data/streaming_data_store.go
+++ streaming_data/streaming_data_store.go
@@ -53,25 +53,15 @@ func (s *StreamingDataStore[TK]) Add(ctx context.Context, key TK) (*Encoder[TK],
 	return newEncoder(ctx, w), nil
 }
 
-// AddIfNotExist adds an item if there is no item matching the key yet.
-// Otherwise, it will do nothing and return false, for not adding the item.
-// This is useful for cases one wants to add an item without creating a duplicate entry.
-func (s *StreamingDataStore[TK]) AddIfNotExist(ctx context.Context, key TK) (*Encoder[TK], error) {
-	if found, err := s.FindOne(ctx, key, false); err != nil || found {
-		return nil, err
-	}
-	return s.Add(ctx, key)
-}
-
 // Remove will delete the item's data chunks given its key.
 func (s *StreamingDataStore[TK]) Remove(ctx context.Context, key TK) (bool, error) {
-	if found, err := s.FindOne(ctx, key, false); err != nil || !found {
+	if found, err := s.FindOne(ctx, key); err != nil || !found {
 		return false, err
 	}
 	return s.RemoveCurrentItem(ctx)
 }
 
-// RemoveCurrentItem will remove the current key/value pair from the store.
+// RemoveCurrentItem will delete the current item's data chunks.
 func (s *StreamingDataStore[TK]) RemoveCurrentItem(ctx context.Context) (bool, error) {
 	key := s.btree.GetCurrentKey().Key
 	keys := make([]StreamingDataKey[TK], 0, 5)
@@ -98,33 +88,31 @@ func (s *StreamingDataStore[TK]) RemoveCurrentItem(ctx context.Context) (bool, e
 	return succeeded, lastErr
 }
 
-// Update finds the item with key and update its value to the value argument.
+// Update finds the item with key and returns an encoder you can use to upload and update the item's data chunks.
 func (s *StreamingDataStore[TK]) Update(ctx context.Context, key TK) (*Encoder[TK], error) {
-	if found, err := s.FindOne(ctx, key, false); err != nil || !found {
+	if found, err := s.FindOne(ctx, key); err != nil || !found {
 		return nil, err
 	}
 	return s.UpdateCurrentItem(ctx)
 }
 
-// UpdateCurrentItem will update the Value of the current item.
-// Key is read-only, thus, no argument for the key.
+// UpdateCurrentItem will return an encoder that will allow you to update the current item's data chunks.
 func (s *StreamingDataStore[TK]) UpdateCurrentItem(ctx context.Context) (*Encoder[TK], error) {
 	w := newWriter[TK](ctx, false, s.btree.GetCurrentKey().Key, s.btree)
 	return newEncoder(ctx, w), nil
 }
 
-// GetCurrentValue returns the current item's value.
+// GetCurrentValue returns the current item's decoder you can use to download the data chunks (or stream it down).
 func (s *StreamingDataStore[TK]) GetCurrentValue(ctx context.Context) (*json.Decoder, error) {
-	r := newReader[TK](ctx, s.btree.GetCurrentKey().Key, s.btree)
+	ck := s.btree.GetCurrentKey()
+	r := newReader[TK](ctx, ck.Key, ck.ChunkIndex, s.btree)
 	return json.NewDecoder(r), nil
 }
 
 // FindOne will search Btree for an item with a given key. Return true if found,
-// otherwise false. firstItemWithKey is useful when there are items with same key.
-// true will position pointer to the first item with the given key,
-// according to key ordering sequence.
-// Use the CurrentKey/CurrentValue to retrieve the "current item" details(key &/or value).
-func (s *StreamingDataStore[TK]) FindOne(ctx context.Context, key TK, firstItemWithKey bool) (bool, error) {
+// otherwise false.
+// Use the CurrentKey/CurrentValue to retrieve the "current item" details(key &/or decoder).
+func (s *StreamingDataStore[TK]) FindOne(ctx context.Context, key TK) (bool, error) {
 	k := StreamingDataKey[TK]{Key: key}
 	return s.btree.FindOne(ctx, k, false)
 }
@@ -146,26 +134,24 @@ func (s *StreamingDataStore[TK]) Last(ctx context.Context) (bool, error) {
 	return s.btree.Last(ctx)
 }
 
-// Next positions the "cursor" to the next item as per key ordering.
+// Next positions the "cursor" to the next item chunk as per key ordering.
 // Use the CurrentKey/CurrentValue to retrieve the "current item" details(key &/or value).
 func (s *StreamingDataStore[TK]) Next(ctx context.Context) (bool, error) {
 	return s.btree.Next(ctx)
 }
 
-// Previous positions the "cursor" to the previous item as per key ordering.
+// Previous positions the "cursor" to the previous item chunk as per key ordering.
 // Use the CurrentKey/CurrentValue to retrieve the "current item" details(key &/or value).
 func (s *StreamingDataStore[TK]) Previous(ctx context.Context) (bool, error) {
 	return s.btree.Previous(ctx)
 }
 
-// IsUnique returns true if B-Tree is specified to store items with Unique keys, otherwise false.
-// Specifying uniqueness base on key makes the B-Tree permanently set. If you want just a temporary
-// unique check during Add of an item, then you can use AddIfNotExist method for that.
+// IsUnique always returns true for Streaming Data Store.
 func (s *StreamingDataStore[TK]) IsUnique() bool {
 	return s.btree.IsUnique()
 }
 
-// Returns the number of items in this B-Tree.
+// Returns the total number of data chunks in this store.
 func (s *StreamingDataStore[TK]) Count() int64 {
 	return s.btree.Count()
 }
