3a21a1c4c60769f8b061025c7b9532108ab613a8
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-02-19 11:18:38 -0800

    Updated StoreInfo comment to better document the fields.

--- store_info.go
+++ store_info.go
@@ -8,8 +8,9 @@ type StoreInfo struct {
 	SlotLength int
 	// IsUnique tells whether key/value pair (items) of this tree should be unique on key.
 	IsUnique bool
-	// IsValueDataInNodeSegment is true if "Value" data is stored in the B-Tree node's data segment.
-	// Otherwise is false.
+	// Set to true if you want "Value" data stored in the B-Tree node's data segment persisted together with the Keys.
+	// Small size "Value" can benefit getting stored in Node's segment, but bigger data needs to be stored in its own segment(false)
+	// not to impact performance.
 	IsValueDataInNodeSegment bool
 	// If true, each Btree Add(..) method call will persist the item value's data to another partition, then on commit,
 	// it will then be a very quick action as item(s) values' data were already saved on backend.
@@ -19,8 +20,10 @@ type StoreInfo struct {
 	// is set to false. Typically set to false if 'IsValueDataActivelyPersisted' is true, as value data is expected
 	// to be huge rendering caching it in Redis to affect Redis performance due to the drastic size of data per item.
 	IsValueDataGloballyCached bool
-	// If true, node load will be balanced by pushing items to sibling nodes if there are vacant slots,
-	// otherwise will not. This feature can be turned off if backend is impacted by the "balancing" act.
+	// If true, during node is full scenario, instead of breaking the node in two to create space, item can get distributed
+	// to sibling nodes with vacant slot(s). This increases density of the nodes.
+	// This feature can be turned off if backend is impacted by the "balancing" act, i.e. - distribution can cause changes
+	// to sibling nodes, thus, may increase I/O unnecessarily.
 	LeafLoadBalancing bool
 	// (optional) Description of the Store.
 	Description string
