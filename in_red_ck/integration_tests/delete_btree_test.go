2c6cc02b4f025ccb27239998dc21ece798906c13
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-03-01 12:32:05 -0800

    Added integration tests (w/ trans logging enabled) for StreamingDataStore.

--- in_red_ck/integration_tests/delete_btree_test.go
+++ in_red_ck/integration_tests/delete_btree_test.go
@@ -12,7 +12,9 @@ func DeleteBTree(t *testing.T) {
 	tableList := []string{
 		"fooStore", "persondb", "twophase", "twophase2", "twophase3",
 		"twophase22", "persondb7", "persondb77", "person2db", "barStore",
-		"tabley", "tablex2", "tablex", "ztab1",
+		"tabley", "tablex2", "tablex", "ztab1", "videoStore",
+		"videoStoreM", "videoStoreD", "videoStoreU",
+		"videoStore2", "videoStore3", "videoStore4", "videoStore5",
 	}
 	for _, tn := range tableList {
 		if err := in_red_ck.RemoveBtree(ctx, tn); err != nil {
