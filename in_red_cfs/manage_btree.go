47a8a32db2c8f20a91cbe9f3a28b974287e536c8
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-08-30 00:01:10 -0700

    Added in_red_ck & in_red_cfs package documentations.

--- in_red_cfs/manage_btree.go
+++ in_red_cfs/manage_btree.go
@@ -1,3 +1,5 @@
+// Package in_red_cfs contains SOP implementations that uses Redis for caching, Cassandra & File System for backend data storage.
+// The Objects Registry is stored in Cassandra and the B-Tree Nodes & their items' value data blobs are stored in the File System.
 package in_red_cfs
 
 import (
