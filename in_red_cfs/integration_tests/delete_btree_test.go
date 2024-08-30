57ebca1d3a140748f631f26922e7369b01f1b54f
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-08-29 22:07:56 -0700

    in_red_cfs: code complete

--- in_red_cfs/integration_tests/delete_btree_test.go
+++ in_red_cfs/integration_tests/delete_btree_test.go
@@ -8,11 +8,11 @@ import (
 
 // Add Test_ prefix if you want to run this test.
 // It drops the blob & registry tables of the B-Tree, thus, the test was removed from the set.
-func Test_DeleteBTree(t *testing.T) {
+func DeleteBTree(t *testing.T) {
 	tableList := []string{
 		"fooStore", "persondb", "twophase", "twophase2", "twophase3",
-		"twophase22", "persondb7", "persondb77", "person2db", "barStore",
-		"tabley", "tablex2", "tablex", "ztab1", "videoStore",
+		"twophase22", "persondb7", "persondb77", "person2db", "barStore1",
+		"barStore2", "tabley", "tablex2", "tablex", "ztab1", "videoStore",
 		"videoStoreM", "videoStoreD", "videoStoreU",
 		"videoStore2", "videoStore3", "videoStore4", "videoStore5",
 	}
