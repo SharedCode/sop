a5f2e7859447859caa703f0c2c648e774376486d
Gerardo Recinto <gerardorecinto@yahoo.com>
2024-09-22 03:32:47 -0700

    Minor test check change.

--- in_red_cfs/integration_tests/transaction_edge_cases_test.go
+++ in_red_cfs/integration_tests/transaction_edge_cases_test.go
@@ -594,7 +594,7 @@ func Test_ConcurrentCommitsComplexDupeAllowed(t *testing.T) {
 	if i < 5 {
 		t.Errorf("Failed, traversing/counting all records, got %d, want 5.", i)
 	}
-	if b3.Count() > 9 {
+	if b3.Count() != 9 {
 		t.Errorf("Failed, traversing/counting all records, got %d, but Count() returned %d.", i, b3.Count())
 	} 
 }
