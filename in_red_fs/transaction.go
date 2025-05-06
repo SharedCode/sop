dc0264ce69d2680902815a60c496344150897c33
Gerardo Recinto <gerardorecinto@Yahoo.com>
2025-05-06 00:10:16 -0700

    Added TransactionOptions paramereter config struct & fixed an issue on a case where Handles CacheDuration not being propagated. (#119)

--- in_red_fs/transaction.go
+++ in_red_fs/transaction.go
@@ -2,7 +2,6 @@ package in_red_fs
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/SharedCode/sop"
 	"github.com/SharedCode/sop/common"
@@ -11,70 +10,73 @@ import (
 )
 
 // NewTransaction is a convenience function to create an enduser facing transaction object that wraps the two phase commit transaction.
-func NewTransaction(storesBaseFolder string, mode sop.TransactionMode, maxTime time.Duration, registryHashModValue fs.HashModValueType, cache sop.Cache, useCacheForFileRegionLocks bool) (sop.Transaction, error) {
-	twoPT, err := NewTwoPhaseCommitTransaction(storesBaseFolder, mode, maxTime, registryHashModValue, cache, useCacheForFileRegionLocks)
+func NewTransaction(to TransationOptions) (sop.Transaction, error) {
+	twoPT, err := NewTwoPhaseCommitTransaction(to)
 	if err != nil {
 		return nil, err
 	}
-	return sop.NewTransaction(mode, twoPT, maxTime, true)
+	return sop.NewTransaction(to.Mode, twoPT, to.MaxTime, true)
 }
 
 // NewTwoPhaseCommitTransaction will instantiate a transaction object for writing(forWriting=true)
 // or for reading(forWriting=false). Pass in -1 on maxTime to default to 15 minutes of max "commit" duration.
-func NewTwoPhaseCommitTransaction(storesBaseFolder string, mode sop.TransactionMode, maxTime time.Duration, registryHashModValue fs.HashModValueType, cache sop.Cache, useCacheForFileRegionLocks bool) (sop.TwoPhaseCommitTransaction, error) {
+func NewTwoPhaseCommitTransaction(to TransationOptions) (sop.TwoPhaseCommitTransaction, error) {
 	if !IsInitialized() {
 		return nil, fmt.Errorf("Redis was not initialized")
 	}
-	if cache == nil {
-		cache = redis.NewClient()
+	if to.Cache == nil {
+		to.Cache = redis.NewClient()
 	}
-	replicationTracker := fs.NewReplicationTracker([]string{storesBaseFolder}, false)
-	sr, err := fs.NewStoreRepository(replicationTracker, nil, cache)
+	replicationTracker := fs.NewReplicationTracker([]string{to.StoresBaseFolder}, false)
+	sr, err := fs.NewStoreRepository(replicationTracker, nil, to.Cache)
 	if err != nil {
 		return nil, err
 	}
-	tl := fs.NewTransactionLog(cache, replicationTracker)
-	return common.NewTwoPhaseCommitTransaction(mode, maxTime, true, fs.NewBlobStore(nil), sr, fs.NewRegistry(mode == sop.ForWriting, registryHashModValue, replicationTracker, cache, useCacheForFileRegionLocks), cache, tl)
+	tl := fs.NewTransactionLog(to.Cache, replicationTracker)
+	return common.NewTwoPhaseCommitTransaction(to.Mode, to.MaxTime, true,
+		fs.NewBlobStore(nil), sr, fs.NewRegistry(to.Mode == sop.ForWriting,
+			to.RegistryHashModValue, replicationTracker, to.Cache, to.UseCacheForFileRegionLocks), to.Cache, tl)
 }
 
 // Create a transaction that supports replication, via custom SOP replicaiton on StoreRepository & Registry and then Erasure Coding on Blob Store.
-func NewTransactionWithReplication(storesBaseFolders []string, mode sop.TransactionMode, maxTime time.Duration, registryHashModValue fs.HashModValueType, erasureConfig map[string]fs.ErasureCodingConfig, cache sop.Cache, useCacheForFileRegionLocks bool) (sop.Transaction, error) {
-	twoPT, err := NewTwoPhaseCommitTransactionWithReplication(storesBaseFolders, mode, maxTime, registryHashModValue, erasureConfig, cache, useCacheForFileRegionLocks)
+func NewTransactionWithReplication(towr TransationOptionsWithReplication) (sop.Transaction, error) {
+	twoPT, err := NewTwoPhaseCommitTransactionWithReplication(towr)
 	if err != nil {
 		return nil, err
 	}
-	return sop.NewTransaction(mode, twoPT, maxTime, true)
+	return sop.NewTransaction(towr.Mode, twoPT, towr.MaxTime, true)
 }
 
 // Create a transaction that supports replication, via custom SOP replicaiton on StoreRepository & Registry and then Erasure Coding on Blob Store.
 // Returns sop.TwoPhaseCommitTransaction type useful for integration with your custom application transaction where code would like to get access to SOP's two phase commit transaction API.
-func NewTwoPhaseCommitTransactionWithReplication(storesBaseFolders []string, mode sop.TransactionMode, maxTime time.Duration, registryHashModValue fs.HashModValueType, erasureConfig map[string]fs.ErasureCodingConfig, cache sop.Cache, useCacheForFileRegionLocks bool) (sop.TwoPhaseCommitTransaction, error) {
-	if erasureConfig == nil {
-		erasureConfig = fs.GetGlobalErasureConfig()
-		if erasureConfig == nil {
+func NewTwoPhaseCommitTransactionWithReplication(towr TransationOptionsWithReplication) (sop.TwoPhaseCommitTransaction, error) {
+	if towr.ErasureConfig == nil {
+		towr.ErasureConfig = fs.GetGlobalErasureConfig()
+		if towr.ErasureConfig == nil {
 			return nil, fmt.Errorf("erasureConfig can't be nil")
 		}
 	}
 	fio := fs.NewDefaultFileIO(fs.DefaultToFilePath)
-	replicationTracker := fs.NewReplicationTracker(storesBaseFolders, true)
-	bs, err := fs.NewBlobStoreWithEC(fio, erasureConfig)
+	replicationTracker := fs.NewReplicationTracker(towr.StoresBaseFolders, true)
+	bs, err := fs.NewBlobStoreWithEC(fio, towr.ErasureConfig)
 	if err != nil {
 		return nil, err
 	}
 	mbsf := fs.NewManageStoreFolder(fio)
-	if cache == nil {
-		cache = redis.NewClient()
+	if towr.Cache == nil {
+		towr.Cache = redis.NewClient()
 	}
 
 	if !IsInitialized() {
 		return nil, fmt.Errorf("Redis was not initialized")
 	}
-	sr, err := fs.NewStoreRepository(replicationTracker, mbsf, cache)
+	sr, err := fs.NewStoreRepository(replicationTracker, mbsf, towr.Cache)
 	if err != nil {
 		return nil, err
 	}
 
-	tl := fs.NewTransactionLog(cache, replicationTracker)
+	tl := fs.NewTransactionLog(towr.Cache, replicationTracker)
 
-	return common.NewTwoPhaseCommitTransaction(mode, maxTime, true, bs, sr, fs.NewRegistry(mode == sop.ForWriting, registryHashModValue, replicationTracker, cache, useCacheForFileRegionLocks), cache, tl)
+	return common.NewTwoPhaseCommitTransaction(towr.Mode, towr.MaxTime, true, bs, sr,
+		fs.NewRegistry(towr.Mode == sop.ForWriting, towr.RegistryHashModValue, replicationTracker, towr.Cache, towr.UseCacheForFileRegionLocks), towr.Cache, tl)
 }
