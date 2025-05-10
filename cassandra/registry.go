643eab3c7b47ec2b8176c3fd5b0af307eed56b1f
Gerardo Recinto <gerardorecinto@yahoo.com>
2025-05-10 14:39:57 -0700

    Err comment change on registry update fetch from cache "retry" flow.

--- cassandra/registry.go
+++ cassandra/registry.go
@@ -83,7 +83,7 @@ func (v *registry) Update(ctx context.Context, allOrNothing bool, storesHandles 
 						}
 					} else {
 						err = &sop.UpdateAllOrNothingError{
-							Err: fmt.Errorf("Registry Update failed, err getting handle %s data from cache", h.LogicalID.String()),
+							Err: fmt.Errorf("Registry Update failed, err getting handle %s data from cache, details: %v", h.LogicalID.String(), err),
 						}
 					}
 					// Unlock the object Keys before return.
