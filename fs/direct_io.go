4589fd070d5d7186c6d18af76a75df9c1614ba84
Gerardo Recinto <gerardorecinto@yahoo.com>
2025-05-14 01:08:13 -0700

    Cleanup only.

--- fs/direct_io.go
+++ fs/direct_io.go
@@ -1,13 +1,9 @@
 package fs
 
 import (
-	"context"
-	"errors"
 	"fmt"
 	"io"
 	"os"
-	"syscall"
-	"time"
 
 	"github.com/ncw/directio"
 )
@@ -20,8 +16,6 @@ type directIO struct {
 const (
 	blockSize = directio.BlockSize
 )
-
-var errBlocked = errors.New("acquiring lock is blocked by another process")
 
 // Instantiate a direct File IO object.
 func newDirectIO() *directIO {
@@ -78,85 +72,6 @@ func (dio *directIO) readAt(block []byte, offset int64) (int, error) {
 		return 0, fmt.Errorf("can't read, there is no opened file")
 	}
 	return dio.file.ReadAt(block, offset)
-}
-
-func (dio *directIO) lockFileRegion(ctx context.Context, offset int64, length int64, timeout time.Duration) error {
-	if dio.file == nil {
-		return fmt.Errorf("can't lock file region, there is no opened file")
-	}
-
-	flock := syscall.Flock_t{
-		Type:   syscall.F_WRLCK,
-		Whence: 0, // SEEK_SET
-		Start:  offset,
-		Len:    length,
-		Pid:    int32(syscall.Getpid()),
-	}
-
-	if timeout <= 0 {
-		return syscall.FcntlFlock(dio.file.Fd(), syscall.F_SETLK, &flock)
-	}
-
-	sleep, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
-	done := make(chan error, 1)
-
-	go func() {
-		err := syscall.FcntlFlock(dio.file.Fd(), syscall.F_SETLKW, &flock)
-		done <- err
-		close(done)
-		close(done)
-	}()
-
-	select {
-	case err := <-done:
-		return err
-	case <-sleep.Done():
-		return errBlocked
-	}
-}
-
-func (dio *directIO) isRegionLocked(ctx context.Context, readWrite bool, offset int64, length int64) (bool, error) {
-	if dio.file == nil {
-		return false, fmt.Errorf("can't check if region is locked, there is no opened file")
-	}
-
-	var t int16 = syscall.F_WRLCK
-	if !readWrite {
-		t = syscall.F_RDLCK
-	}
-	flock := syscall.Flock_t{
-		Type:   t,
-		Start:  offset,
-		Len:    length,
-		Pid:    0,
-		Whence: 0,
-	}
-
-	err := syscall.FcntlFlock(dio.file.Fd(), syscall.F_GETLK, &flock)
-	if err != nil {
-		return false, err
-	}
-
-	// If lock.Type is F_UNLCK, no lock exists
-	return flock.Type != syscall.F_UNLCK, nil
-}
-
-func (dio *directIO) unlockFileRegion(offset int64, length int64) error {
-	if dio.file == nil {
-		return fmt.Errorf("can't unlock file region, there is no opened file")
-	}
-
-	flock := syscall.Flock_t{
-		Type:   syscall.F_UNLCK, // Unlock
-		Start:  offset,
-		Len:    length,
-		Whence: 0, // SEEK_SET
-		Pid:    int32(syscall.Getpid()),
-	}
-
-	return syscall.FcntlFlock(dio.file.Fd(), syscall.F_SETLK, &flock)
 }
 
 func (dio *directIO) close() error {
