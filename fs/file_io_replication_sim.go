06610887111517d3e3f26fcf2401d36dfccf9b6c
Gerardo Recinto <gerardorecinto@Yahoo.com>
2025-06-01 16:40:50 -0700

    In red fs: Preparation before implementing the 2nd half of StoreRepository & Registry replication (handling failures, failover, etc...). (#182)
    * Preparation before implementing the 2nd half of StoreRepository & Registry replication (handling failures, failover, etc...).
    
    * same.
    
    * same.

---
+++ fs/file_io_replication_sim.go
@@ -0,0 +1,39 @@
+package fs
+
+import (
+	"os"
+
+	"github.com/SharedCode/sop"
+)
+
+type fileIOReplicationSimulator struct {
+}
+
+func newFileIOReplicationSim() *fileIOReplicationSimulator {
+	return &fileIOReplicationSimulator{}
+}
+
+func (sim *fileIOReplicationSimulator) ToFilePath(basePath string, id sop.UUID) string {
+	return ""
+}
+
+func (sim *fileIOReplicationSimulator) WriteFile(name string, data []byte, perm os.FileMode) error {
+	return nil
+}
+func (sim *fileIOReplicationSimulator) ReadFile(name string) ([]byte, error) {
+	return nil, nil
+}
+func (sim *fileIOReplicationSimulator) Remove(name string) error {
+	return nil
+}
+func (sim *fileIOReplicationSimulator) Exists(path string) bool {
+	return true
+}
+
+// Directory API.
+func (sim *fileIOReplicationSimulator) RemoveAll(path string) error {
+	return nil
+}
+func (sim *fileIOReplicationSimulator) MkdirAll(path string, perm os.FileMode) error {
+	return nil
+}
