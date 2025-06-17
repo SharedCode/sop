7f96782031dca7c02d727b46144c0258be821ae6
Gerardo Recinto <gerardorecinto@yahoo.com>
2025-06-16 17:37:35 -0700

    in_red_fs: refinements on acquireLocks logic.

--- common/transaction_logger.go
+++ common/transaction_logger.go
@@ -147,6 +147,7 @@ func (tl *transactionLog) doPriorityRollbacks(ctx context.Context, t *Transactio
 				for i := range uhAndrh {
 					for ii := range uhAndrh[i].IDs {
 						if !(uhAndrh[i].IDs[ii].Version == cuhAndrh[i].IDs[ii].Version || uhAndrh[i].IDs[ii].Version+1 == cuhAndrh[i].IDs[ii].Version) {
+							t.l2Cache.Unlock(ctx, lks)
 							// Version in Registry had gone past the value we can repair, 'just trigger a failover.
 							return false, sop.Error{
 								Code:     sop.RestoreRegistryFileSectorFailure,
@@ -212,15 +213,26 @@ func (tl *transactionLog) acquireLocks(ctx context.Context, t *Transaction, tid 
 		return keys, nil
 	} else if !ownerTID.IsNil() {
 		if ownerTID.Compare(tid) != 0 {
+			t.l2Cache.Unlock(ctx, keys)
 			return keys, sop.Error{
 				Code: sop.RestoreRegistryFileSectorFailure,
 				Err:  fmt.Errorf("key(s) is locked by another transaction %s, 'can't acquire lock to restore registry", ownerTID.String()),
 			}
 		}
+		// Attempt to acquire ownership of the keys' locks from the dead transaction.
+
+		// Clone to a new set so we can still unlock w/ the untainted copy.
+		keysCopy := make([]*sop.LockKey, len(keys))
+		for i := range keys {
+			copy := *keys[i]
+			keysCopy[i] = &copy
+		}
 		for i := range keys {
 			if ok, tid2, err := t.l2Cache.GetEx(ctx, keys[i].Key, defaultLockDuration); ok {
 				if tid.String() == tid2 {
+					// Acquire the LockID of the dead transaction.
 					keys[i].LockID = tid
+					keys[i].IsLockOwner = true
 				} else {
 					return keys, sop.Error{
 						Code: sop.RestoreRegistryFileSectorFailure,
@@ -228,21 +240,15 @@ func (tl *transactionLog) acquireLocks(ctx context.Context, t *Transaction, tid 
 					}
 				}
 			} else if err != nil {
+				// Unlock any key that got locked by lock call above, if there is any.
+				t.l2Cache.Unlock(ctx, keysCopy)
 				return keys, err
 			}
 		}
-		defer t.l2Cache.Unlock(ctx, keys)
-		if ok, tid3, err := t.l2Cache.Lock(ctx, defaultLockDuration, keys); ok {
-			return keys, nil
-		} else if err != nil {
-			return keys, err
-		} else {
-			return keys, sop.Error{
-				Code: sop.RestoreRegistryFileSectorFailure,
-				Err:  fmt.Errorf("key(s) is locked by another transaction %s, 'can't acquire lock to restore registry", tid3),
-			}
-		}
+		// At this point, it is as good as we have achieved full lock on the set of keys, because they acquired lock ownership.
+		return keys, nil
 	} else {
+		t.l2Cache.Unlock(ctx, keys)
 		return keys, err
 	}
 }
