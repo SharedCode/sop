65cd920d81a537b9b666deec62bbd1d0f0113129
Gerardo Recinto <gerardorecinto@yahoo.com>
2025-06-23 19:51:18 -0700

    SOP for Python related.

---
+++ jsondb/python/btree.py
@@ -0,0 +1,76 @@
+import uuid
+
+from typing import TypeVar, Generic
+
+# Define TypeVars 'TK' & 'TV' to represent Key & Value generic types.
+TK = TypeVar('TK')
+TV = TypeVar('TV')
+
+from datetime import timedelta
+
+from enum import Enum
+class ValueDataSize(Enum):
+    Small = 0
+    Medium = 1
+    Big = 2
+
+MIN_CACHE_DURATION = 5*timedelta.minute
+
+class CacheConfig:
+    """
+    Cache config specify the options available for caching in B-tree.
+    """
+    def __init__(self, cache_duration: timedelta, is_ttl: bool):
+        if cache_duration > 0 and cache_duration < MIN_CACHE_DURATION:
+            cache_duration = MIN_CACHE_DURATION
+        if cache_duration == 0 and is_ttl:
+            is_ttl = False
+        self.registry_cache_duration = cache_duration
+        self.is_registry_cache_ttl = is_ttl
+        self.node_cache_duration = cache_duration
+        self.is_node_cache_ttl = is_ttl
+        self.store_info_cache_duration = cache_duration
+        self.is_store_info_cache_ttl = is_ttl
+        self.value_data_cache_duration = cache_duration
+        self.is_value_data_cache_ttl = is_ttl
+
+class BtreeOptions:
+    """
+    Btree options specify the options available for making a B-tree.
+    """
+    def __init__(self, name: str, is_unique: bool, slot_length: int, desc: str, value_size: ValueDataSize,
+                 cache_config: CacheConfig):
+        self.name = name
+        self.is_unique = is_unique
+        self.slot_length = slot_length
+        self.desc = desc
+        # Defaults to Small data size.
+        self.is_value_data_in_node_segment = True
+        self.is_value_data_globally_cached = False
+        self.is_value_data_actively_persisted = False
+        if value_size == ValueDataSize.Medium:
+            self.is_value_data_in_node_segment = False
+            self.is_value_data_globally_cached = True
+        if value_size == ValueDataSize.Big:
+            self.is_value_data_in_node_segment = False
+            self.is_value_data_globally_cached = False
+            self.is_value_data_actively_persisted = True
+        self.cache_config = cache_config
+
+class Btree(Generic[TK, TV]):
+
+    # @staticmethod
+    # def comparerBuilder()
+
+    @staticmethod
+    def new(options: BtreeOptions, ):
+         Btree()
+    
+    @staticmethod
+    def open(name: str):
+        return self
+
+    @classmethod
+    def add():
+        return
+    
--- jsondb/python/transaction.py
+++ jsondb/python/transaction.py
@@ -1,25 +1,49 @@
-import datetime
 
-from typing import Dict
+from datetime import timedelta
 from enum import Enum
 class TransactionMode(Enum):
     NoCheck = 0
     ForWriting = 1
     ForReading = 2
 
+# 250, should generate 1MB file segment. Formula: 250 X 4096 = 1MB
+# Given a 50 slot size per node, should be able to manage 825,000 B-Tree items (key/value pairs).
+#
+# Formula: 250 * 66 * 50 = 825,000
+# Or if you use 100 slot size per node, 'will give you 1,650,000 items, or assuming you have about 65%
+# b-tree utilization, 1,072,500 usable space.
+MIN_HASH_MOD_VALUE = 250
+# 750k, should generate 3GB file segment.  Formula: 750k X 4096 = 3GB
+MAX_HASH_MOD_VALUE = 750000
+
 class ErasureCodingConfig:
-    def __init__(self, data_shards_count: int, parity_shards_count: int, base_folder_paths_across_drives: list, repair_corrupted_shards: bool):
+    def __init__(self, data_shards_count: int, parity_shards_count: int, base_folder_paths_across_drives: list[str], repair_corrupted_shards: bool):
         self.data_shards_count = data_shards_count
         self.parity_shards_count = parity_shards_count
         self.base_folder_paths_across_drives = base_folder_paths_across_drives
         self.repair_corrupted_shards = repair_corrupted_shards
 
 class TransationOptions:
-    def __init__(self, store_folder: str, mode: TransactionMode, max_time: datetime.datetime.minute, registry_hash_mod: int, erasure_config: Dict[str, ErasureCodingConfig]):
+    glolbal_erasure_config: ErasureCodingConfig
+    def __init__(self, stores_folders: str, mode: TransactionMode, max_time: timedelta, registry_hash_mod: int, erasure_config: dict[str, ErasureCodingConfig]):
+        if erasure_config == None:
+            erasure_config = TransationOptions.glolbal_erasure_config
+        if len(stores_folders) != 2:
+            raise "'stores_folders' need to be array of two strings(drive/folder paths)"
+
+        if registry_hash_mod < MIN_HASH_MOD_VALUE:
+            registry_hash_mod = MIN_HASH_MOD_VALUE
+        if registry_hash_mod > MAX_HASH_MOD_VALUE:
+            registry_hash_mod = MAX_HASH_MOD_VALUE
+
+        # Default to 15 minute commit time.
+        if max_time < 0:
+            max_time = 15*timedelta.minutes
+
         # Base folder where the Stores (registry, blob & store repository) subdirectories & files
         # will be created in. This is expected to be two element array, the 2nd element specifies
         # a 2nd folder for use in SOP replication.
-        self.store_folder = store_folder
+        self.store_folders = stores_folders
         # Transaction Mode can be Read-only or Read-Write.
         self.mode = mode
         # Transaction maximum "commit" time. If commits takes longer than this then transaction will roll back.
@@ -28,3 +52,20 @@ class TransationOptions:
         self.registry_hash_mod = registry_hash_mod
         # Erasure Config contains config data useful for Erasure Coding based file IO (& replication).
         self.erasure_config = erasure_config
+
+class Transaction:
+    def __init__(self, options: TransationOptions):
+        self.options = options
+
+    @classmethod
+    def begin():
+        return
+
+    @classmethod
+    def commit():
+        return
+
+    @classmethod
+    def rollback():
+        return
+
