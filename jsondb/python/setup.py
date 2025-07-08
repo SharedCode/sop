3cd01f61de0a689eb2e08544eebabdc4da3d47b0
Gerardo Recinto <gerardorecinto@yahoo.com>
2025-07-08 07:30:26 -0700

    SOP for Python code library (json package) binary for darwin amd64, checking in a sample. sop/jsondb/main/build.sh can generate the full set cross compiled binaries. :)

--- jsondb/python/setup.py
+++ jsondb/python/setup.py
@@ -1,12 +1,15 @@
 from sop import __version__
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='sop',
     version=__version__,
     packages=['sop'],
     package_data={
-        'sop': ['libjsondb_amd64darwin.dylib'], # Relative path within your package
+        'sop': ['libjsondb_amd64darwin.dylib', 'libjsondb_amd64darwin.h'], # Relative path within your package
     },
     include_package_data=True, # Important to ensure package_data is included
 )
+
+# Sample cmdline to build wheel:
+# python3 setup.py sdist bdist_wheel
